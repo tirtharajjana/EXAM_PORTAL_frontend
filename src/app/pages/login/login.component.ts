bbd6e7acc18e490c2094958e45669ebfa34354bc
Tirtharaj <tirtharajjana615@gmail.com>
2022-08-31 17:39:42 +0530

    Login at FrontEnd

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,5 @@
+import { LoginService } from './../../services/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    username: '',
+    password: ''
+  }
+
+
+
+  constructor(private snack: MatSnackBar, private loginService: LoginService) { }
+
+  formSubmit() {
+    // alert(JSON.stringify(this.loginData));
+    if (this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.snack.open('Username is required', '', { duration: 2000 });
+      return;
+    }
+    if (this.loginData.password.trim() == '' || this.loginData.password == null) {
+      this.snack.open('Password is required', '', { duration: 2000 });
+      return;
+    }
+
+    //request to server to generate token
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data: any) => {
+        console.log(data);
+        console.log("token generated");
+      },
+      (err) => {
+        console.log(err);
+        this.snack.open('Something went wrong', '', { duration: 2000 });
+      }
+    )
+
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseURL from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  //generate token
+  public generateToken(loginData: any) {
+    return this.http.post(`${baseURL}/generate-token`, loginData);
+  }
+
+  //login user: set token in local storage
+  public loginUser(token: string) {
+    localStorage.setItem('token', token);
+    return true;
+  }
+
+  //isLogin: user is logged in or not
+  public isLoggedin() {
+    if (localStorage.getItem('token')) {
+      return true;
+    }
+    return false;
+  }
+
+  //logout user: remove token from local storage
+  public logoutUser() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  //get token from local storage
+  public getToken() {
+    return localStorage.getItem('token');
+  }
+
+  //set user details in local storage
+  public setUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  //get user details from local storage
+  public getUser() {
+    let userString = localStorage.getItem('user');
+    if (userString) {
+      return JSON.parse(userString);
+    } else {
+      this.logoutUser();
+      return null;
+    }
+  }
+
+  //get user role
+  public getUserRole() {
+    let user = this.getUser();
+    if (user) {
+      return user.authorities[0].authority;
+    } else {
+      return null;
+    }
+  }
+
+}
